38a70efacc536c01a6dcaeb3ac94deb4de1f9d80
Adrien Husson <pangel.neu@gmail.com>
2015-03-19 17:05:44 +0100

    Fix lone literal detection bug

--- sat.ml
+++ sat.ml
@@ -68,6 +68,7 @@ module S : Solver = struct
   let neg l = (fst l, not (snd l))
   let empty_valuation = ValMap.empty
   let assign l setA gamma = ValMap.add (fst l) (snd l,setA) gamma
+  let defined l gamma = ValMap.mem (fst l) gamma
 
   let union = Lits.union
   let mem = Lits.mem
@@ -97,15 +98,18 @@ module S : Solver = struct
     *    Otherwise, stop *)
   let rec clean gamma l setA delta =
 
-    let rec bla gamma l lits setA delta =
-      delta |> bcp l setA lits |> assume (assign l setA gamma)
+    let rec recursor gamma l lits setA delta =
+      if (defined l gamma) then
+        assume gamma (lits,delta)
+      else
+        delta |> bcp l setA lits |> assume (assign l setA gamma)
 
     and assume gamma (lits,delta) =
       match lits with
-      | (l,setA)::ls -> bla gamma l ls setA delta
+      | (l,setA)::ls -> recursor gamma l ls setA delta
       | [] -> (gamma,delta)
 
-    in bla gamma l [] setA delta
+    in recursor gamma l [] setA delta
 
   let trivially_true = function [] -> true | _ -> false
 
